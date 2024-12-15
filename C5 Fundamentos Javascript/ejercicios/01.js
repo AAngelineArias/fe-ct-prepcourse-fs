2626aa62a1c95899b649264b466a5233f72f22d9
Angeline <angelinearias17@gmail.com>
2024-12-15 12:58:48 -0500

    First commit: HTML file

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,8 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = null;
+let miCadena = null; 
+miCadena = "Hola, henry"; 
+console.log(miCadena); 
 
 module.exports = nuevoString;
