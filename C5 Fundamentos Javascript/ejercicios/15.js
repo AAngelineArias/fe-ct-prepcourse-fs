49dba4877faba9efe8e4c6a821372f9edce1988e
Angeline <angelinearias17@gmail.com>
2024-12-16 15:59:32 -0500

    ejercicios

--- C5 Fundamentos Javascript/ejercicios/15.js
+++ C5 Fundamentos Javascript/ejercicios/15.js
@@ -6,6 +6,13 @@ function esPar(num) {
   // 14 ---> true
   // 15 ---> false
   // Tu código:
+  if (num % 2 === 0) {
+    return true; // Retorna true si el número es par
+  } else {
+    return false; // Retorna false si el número es impar
+  }
+ 
 }
+console.log(esPar(7));
 
 module.exports = esPar;
